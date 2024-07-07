be652af581f9f2ea8fe2b43a5c594f9d1c6b4f71
Aditya-3101 <adityadhayfule3101@gmail.com>
2024-07-07 15:57:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const { listenerCount } = require("process");
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 4000 || process.env.PORT;
+
+//let videoId;
+
+app.get("/download",async (req, res) => {
+  try{
+    const url = req.query.url;
+    const videoId = await ytdl.getURLVideoID(url)
+    const metaInfo = await ytdl.getInfo(url)
+
+    let data = {
+      url:"https://www.youtube.com/embed/"+videoId,
+      info:metaInfo.formats
+    }
+    return res.send(data)
+  }catch(err){
+    return res.status(500)
+  }
+});
+
+app.listen(4000, () => {
+  console.log(`server is running on port ${PORT}`);
+});
